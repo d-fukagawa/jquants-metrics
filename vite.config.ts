70f2898a81a8581ee8e979b091706ac33b20f6eb
d-fukagawa <dfukagawa@gmail.com>
2026-09-09 14:14:10 +0900

    chore: exclude temporary files from verification

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import build from '@hono/vite-build/cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [
@@ -10,5 +10,8 @@ export default defineConfig({
       adapter,
       entry: 'src/index.tsx'
     })
-  ]
+  ],
+  test: {
+    exclude: [...configDefaults.exclude, 'tmp/**']
+  }
 })
